5b74295ab80d4724a00cf1b060bc446e91d79f9a
Mona Khalaf <m__k24@hotmail.com>
2024-07-08 13:11:21 +0200

    Update app.py
    Display an image and align it to the center.
    Show bold text
    display the prediction in blue colour

--- app.py
+++ app.py
@@ -2,22 +2,28 @@ import streamlit as st
 import requests
 from PIL import Image
 
+
 '''
-# CallTagger frontend
+# CallTagger Application
 '''
 
 st.markdown('''
 CallTagger is an application that you can use to classify your text/transcript
 ''')
 
-image = Image.open('callCenter.jpg')
+image = Image.open('../CallTagger-interface/images/callCenter_girl.png')
 
-st.image(image, caption='Call Center Service')
+# Images (and most elements in general) are always aligned to the left, to align the image to the center we can use:
+left_co, cent_co,last_co = st.columns(3)
+with cent_co:
+    st.image(image, caption='Welcome to our Call Center Service')
+
+# st.image(image, caption='Welcome to our Call Center Service')
 
 url = 'https://mvp-calltagger-sqsyyuiuaq-ew.a.run.app/predict'
 
 
-user_transcript = st.text_area('Enter a text to be analyzed 👇', max_chars=3000)
+user_transcript = st.text_area('**Please enter a text to be analyzed and categorized**👇', max_chars=3000)
 # st.write("You entered: ", user_transcript)
 
 params = {'transcript': user_transcript}
@@ -32,7 +38,8 @@ if button:
     st.write('Click to get new prediction')
     # st.write('Further clicks are not visible but are executed')
 
-    # display the prediction to the user
-    st.write('The predicted label is: ', str(response['predicted_label']))
+    # display the prediction to the user in **bold** and the label in blue
+    # st.write('**The predicted label is: **', str(response['predicted_label']))
+    st.write(f'**The predicted label is: :blue[{str(response["predicted_label"])}]**')
 else:
     st.write('Click to get label')
